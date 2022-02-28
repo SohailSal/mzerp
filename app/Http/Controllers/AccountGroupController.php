06d3656bfd14c885d74740152e8acf6b97b1dcde
MuhammadHaris55 <hkhn567@gmail.com>
2022-02-28 17:02:38 +0500

    working on dynamic dropdowns logic-accountGroup

--- app/Http/Controllers/AccountGroupController.php
+++ app/Http/Controllers/AccountGroupController.php
@@ -6,10 +6,12 @@ use App\Models\Account;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
 use App\Models\AccountGroup;
+use App\Models\AccountType;
 use App\Models\Company;
 use Database\Seeders\AccountSeeder;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request as Req;
 
 class AccountGroupController extends Controller
 {
@@ -102,13 +104,21 @@ class AccountGroupController extends Controller
         $types = \App\Models\AccountType::all()->map->only('id', 'name');
         $first = \App\Models\AccountType::all('id', 'name')->first();
 
+        $account_groups[0] = \App\Models\AccountGroup::where('type_id', $first->id)->get();
+        $account_group[0] = \App\Models\AccountGroup::where('type_id', $first->id)->first();
+        // dd($account_group);
+
         return Inertia::render('AccountGroups/Create', [
-            'types' => $types, 'first' => $first,
+            'types' => $types,
+            'first' => $first,
+            'account_groups' => $account_groups,
+            'account_group' => $account_group,
         ]);
     }
 
-    public function store()
+    public function store(Req $request)
     {
+        dd($request);
         Request::validate([
             'type' => ['required'],
             'name' => ['required'],
@@ -159,4 +169,66 @@ class AccountGroupController extends Controller
         $accountgroup->delete();
         return Redirect::back()->with('success', 'Account Group deleted.');
     }
+
+
+    public function account_type_ch(Req $request)
+    {
+        // dd($request);
+        $accountgroups[0] =  AccountGroup::where('type_id', $request->type)->get();
+        $accountgroup[0] =  AccountGroup::where('type_id', $request->type)->first();
+    
+        $types = AccountType::all()->map->only('id', 'name');
+        $first = AccountType::where('id', $request->type)->first();
+
+
+        return Inertia::render('AccountGroups/Create', [
+            'types' => $types, 'first' => $first,
+            'name' => $request->name,
+            'account_groups' => $accountgroups,
+            'account_group' => $accountgroup,
+        ]);
+    }
+    public function account_group_ch(Req $request)
+    {
+        
+        // $accountgroups = null;
+        // $accountgroup = null;
+        $types = AccountType::all()->map->only('id', 'name');
+        $first = AccountType::where('id', $request->type)->first();
+
+        // dd($first);
+
+        $accountgroups[0] = AccountGroup::where('type_id', $request->type)->get();
+       
+        // $accountgroup[0] = AccountGroup::where('type_id', $request->type)->first();
+        $accountgroup[0] = AccountGroup::where('id', $request->acc_groups[0]['acc_group'])->first();
+        
+        for ($i = 0; $i < count($request->acc_groups); $i++) {
+            // dd($i);
+            // dd($request->acc_groups[$i]['acc_group']);
+            if(AccountGroup::where('parent_id', $request->acc_groups[$i]['acc_group'])->first())
+            {
+                $accountgroups[$i + 1] =  \App\Models\AccountGroup::where('parent_id', $request->acc_groups[$i]['acc_group'])->get();
+                $accountgroup[$i + 1] =  \App\Models\AccountGroup::where('parent_id', $request->acc_groups[$i]['acc_group'])->first();
+            }
+            // else {
+                // array_pop($accountgroups);
+                // $accountgroups[] = count($accountgroups) - 1;
+            // }
+        }
+    //    dd(count($request->acc_groups));
+
+        //  dd($accountgroups);
+        // dd($accountgroups);
+
+        // dd($accountgroup);
+        return Inertia::render('AccountGroups/Create', [
+            'types' => $types,
+            'first' => $first,
+            'account_groups' => $accountgroups,
+            'account_group' => $accountgroup,
+            'name' => $request->name,
+        ]);
+    }
+
 }
